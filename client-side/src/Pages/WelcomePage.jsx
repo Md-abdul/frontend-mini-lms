3b63aaaefdb58ddc1ca49a2491fecbfef9e97f08
Md-abdul <mdabdulq62@gmail.com>
2024-06-09 17:05:00 +0530

    deployment

--- client-side/src/Pages/WelcomePage.jsx
+++ client-side/src/Pages/WelcomePage.jsx
@@ -11,46 +11,60 @@ export const WelcomePage = () => {
       boxShadow={
         "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px"
       }
-      w="80%"
+      w={{ base: "90%", md: "60%", lg: "80%" }}
       m="auto"
-      height="95vh"
+      height={{ base: "auto", md: "95vh" }}
       display="flex"
       alignItems="center"
       bg={"white"}
-      borderRadius={'20px'}
+      borderRadius={"20px"}
+      flexDirection={{ base: "column", lg: "row" }}
     >
       <Flex
         width="100%"
         justifyContent="space-between"
         alignItems="center"
         p={4}
+        flexDirection={{ base: "column", md: "row" }}
       >
         {/* Left Side Content */}
         <Box width="50%">
-          <Image src={cermonyCap} alt="Logo" mb={8} />
-          <Flex>
-            <Link to={'/login'}>
-            <Button
-              colorScheme="black"
-              aria-label="More details"
-              size="lg"
-              mr={4}
-              bgGradient="linear(to-b, #ADBBDE, #B6BCCA )" //\\
-              color={"black"}
-              _hover={{ bg: "cyan.400" }}
-            >
-              Login as a Student
-            </Button></Link>
-            <Link to={'/teacherlogin'}>
-            <Button
-              variant="outline"
-              colorScheme="white"
-              aria-label="More details"
-              size="lg"
-              _hover={{ bg: "cyan.400" }}
-            >
-              Login as a Teacher
-            </Button>
+          <Image
+            src={cermonyCap}
+            alt="Logo"
+            mb={8}
+            m={{ base: "auto", lg: "0" }}
+          />
+          <Flex
+            justifyContent={{ base: "center", lg: "flex-start" }}
+            flexDirection={{ base: "column", md: "row" }}
+            alignItems={{ base: "center", md: "flex-start" }}
+          >
+            <Link to={"/login"}>
+              <Button
+                colorScheme="black"
+                aria-label="More details"
+                size="lg"
+                mr={{ base: 0, md: 4 }}
+                mb={{ base: 4, md: 0 }}
+                bgGradient="linear(to-b, #ADBBDE, #B6BCCA )" //\\
+                color={"black"}
+                _hover={{ bg: "cyan.400" }}
+              >
+                Login as a Student
+              </Button>
+            </Link>
+            <Link to={"/teacherlogin"}>
+              <Button
+                variant="outline"
+                colorScheme="white"
+                aria-label="More details"
+                size="lg"
+                _hover={{ bg: "cyan.400" }}
+                mb={{ base: 4, md: 0 }}
+              >
+                Login as a Teacher
+              </Button>
             </Link>
           </Flex>
         </Box>
@@ -58,7 +72,7 @@ export const WelcomePage = () => {
         {/* Right Side Content */}
         <Box
           position="relative"
-          width="70%"
+          width={{ base: "100%", lg: "70%" }}
           display="flex"
           justifyContent="center"
           alignItems="center"
@@ -76,7 +90,7 @@ export const WelcomePage = () => {
             top="50%"
             left="60%"
             transform="translate(-50%, -50%)"
-            width="70%"
+            width={{ base: "60%", md: "70%", lg: "80%" }}
             height="auto"
           />
         </Box>
@@ -84,3 +98,4 @@ export const WelcomePage = () => {
     </Box>
   );
 };
+
